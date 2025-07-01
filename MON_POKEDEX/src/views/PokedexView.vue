5b2b3561f22112cfb2ffc86a5f85ac53a26d0851
malick1302 <malick.baguiri@epitech.eu>
2025-07-01 22:51:53 +0200

    ADD; Pagination to PokedexView.vue

--- MON_POKEDEX/src/views/PokedexView.vue
+++ MON_POKEDEX/src/views/PokedexView.vue
@@ -1,15 +1,46 @@
 <script setup>
 import { ref, onMounted } from 'vue'
-import { fetchPokemonList, fetchPokemonDetails } from '@/api/pokeapi'
+import { fetchPokemonDetails, fetchPokemonList } from '@/api/pokeapi'
+
 
 const pokemons = ref([])
+const offset = ref(0)
+const limit = 20
+const hasNext = ref(false)
+const hasPrev = ref(false)
 
-onMounted(async () => {
-  const list = await fetchPokemonList(0, 10)
-  const pokemonDetailRequests = list.map(p => fetchPokemonDetails(p.url))
-  pokemons.value = await Promise.all(pokemonDetailRequests)
-})
+async function loadPokemons() {
+    const data = await fetchPokemonList(offset.value, limit)
+
+    hasNext.value = !!data.next
+    hasPrev.value = !!data.previous
+
+    if (!data.results) {
+      pokemons.value = []
+      return
+    }
+    const detailRequests = data.results.map(p => fetchPokemonDetails(p.url))
+    pokemons.value = await Promise.all(detailRequests)
+    
+}
+
+function nextPage(){
+    offset.value += limit
+    loadPokemons()
+}
+
+function prevPage(){
+    if (offset.value >= limit) {
+    offset.value -= limit
+    loadPokemons()
+    }
+}
+
+onMounted(loadPokemons)
+
 </script>
+
+
 
 <template>
   <main>
@@ -21,5 +52,10 @@ onMounted(async () => {
     {{ p.name }}
   </li>
 </ul>
+<div >
+  <button @click="prevPage" :disabled="!hasPrev">Précédent</button>
+  <button @click="nextPage" :disabled="!hasNext">Suivant</button>
+
+</div>
   </main>
 </template>

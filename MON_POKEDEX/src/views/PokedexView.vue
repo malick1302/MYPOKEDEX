1632b56cb858dc32c292b0e85f92eddb3b5a217b
malick1302 <malick.baguiri@epitech.eu>
2025-07-01 22:02:23 +0200

    ADD: fetch pokemon list with pictures and names

--- MON_POKEDEX/src/views/PokedexView.vue
+++ MON_POKEDEX/src/views/PokedexView.vue
@@ -1,8 +1,25 @@
 <script setup>
+import { ref, onMounted } from 'vue'
+import { fetchPokemonList, fetchPokemonDetails } from '@/api/pokeapi'
+
+const pokemons = ref([])
+
+onMounted(async () => {
+  const list = await fetchPokemonList(0, 10)
+  const pokemonDetailRequests = list.map(p => fetchPokemonDetails(p.url))
+  pokemons.value = await Promise.all(pokemonDetailRequests)
+})
 </script>
 
 <template>
   <main>
 <h1>MON POKEDEX</h1>
+
+<ul>
+  <li v-for="p in pokemons" :key="p.id">
+    <img :src="p.sprite" alt="sprite" />
+    {{ p.name }}
+  </li>
+</ul>
   </main>
 </template>

6bad5d3916f6f7069271706c0786b0dd750430f8
malick1302 <malick.baguiri@epitech.eu>
2025-07-02 10:38:06 +0200

    ADD: pagination in 20/50/100/all

--- MON_POKEDEX/src/views/DetailView.vue
+++ MON_POKEDEX/src/views/DetailView.vue
@@ -1,13 +1,15 @@
 <script setup>
 import { ref, onMounted, watch } from 'vue'
-import { useRoute } from "vue-router"
-import { fetchPokemonId, fetchEvolutionChainByName} from '@/api/pokeapi'
+import { useRoute } from 'vue-router'
+import { fetchPokemonId, fetchEvolutionChainByName } from '@/api/pokeapi'
 import { useFavoriteStore } from '@/stores/favoriteStore'
 
 const route = useRoute()
 const pokemon = ref(null)
 const evolutionChain = ref([])
 const evolutionDetails = ref([])
+
+const favoriteStore = useFavoriteStore()
 
 async function fetchEvolutionDetails(chain) {
   const details = await Promise.all(chain.map(async (name) => {
@@ -37,41 +39,43 @@ watch(() => route.params.id, (newId) => {
   loadData(newId)
 })
 
+function toggleFavorite() {
+  if (pokemon.value) {
+    favoriteStore.toggleFavorite(pokemon.value.name)
+  }
+}
 
 </script>
 
-
 <template>
   <div v-if="pokemon">
-  <h1>{{ pokemon.name }}</h1>
-  <img :src="pokemon.sprites.front_default" alt="Image ou Gif de {{ pokemon.name }}" />
-  
-  
-  <div v-if="evolutionChain.length">
-    <h2>Chaîne d'évolution :</h2>
-    <ul>
-  <li v-for="evo in evolutionDetails" :key="evo.name">
-    <p>{{ evo.name }}</p>
-   <router-link :to="{ name: 'pokemon-info', params: { id: evo.name } }">
-  <img :src="evo.sprite" :alt="`Image de ${evo.name}`" style="cursor: pointer;" />
-</router-link>
-  </li>
-</ul>
+    <h1>{{ pokemon.name }}</h1>
+    <img :src="pokemon.sprites.front_default" :alt="`Image de ${pokemon.name}`" />
+    
+    <button @click="toggleFavorite" class="mt-2 px-4 py-2 bg-blue-500 text-white rounded">
+      {{ favoriteStore.isFavorite(pokemon.name) ? 'Retirer des favoris' : 'Ajouter aux favoris' }}
+    </button>
 
-</div>
+    <div v-if="evolutionChain.length" class="mt-4">
+      <h2>Chaîne d'évolution :</h2>
+      <ul>
+        <li v-for="evo in evolutionDetails" :key="evo.name">
+          <p>{{ evo.name }}</p>
+          <router-link :to="{ name: 'pokemon-info', params: { id: evo.name } }">
+            <img :src="evo.sprite" :alt="`Image de ${evo.name}`" style="cursor: pointer;" />
+          </router-link>
+        </li>
+      </ul>
+    </div>
 
-
-  <p>ID: {{ pokemon.id}}</p>
-  <p>Types: <span v-for="typeInfo in pokemon.types" :key="typeInfo.slot">
-    {{ typeInfo.type.name }}<span v-if="typeInfo.slot < pokemon.types.length">, </span>
-  </span>
-  </p>
-  <p> Poids: {{ pokemon.weight }} </p>
+    <p>ID: {{ pokemon.id }}</p>
+    <p>Types: 
+      <span v-for="typeInfo in pokemon.types" :key="typeInfo.slot">
+        {{ typeInfo.type.name }}<span v-if="typeInfo.slot < pokemon.types.length">, </span>
+      </span>
+    </p>
+    <p>Poids: {{ pokemon.weight }}</p>
     <p>Taille: {{ pokemon.height }}</p>
-</div>
+  </div>
   <p v-else>Chargement...</p>
-
-
 </template>
-
- 
